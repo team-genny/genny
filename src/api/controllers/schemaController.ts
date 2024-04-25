58b37c1e5ca88f848b3cb8595d55b82d77b3ed4a
Srijan <srijan.vaddadi@gmail.com>
2024-04-25 22:00:48 +0100

    add functionality to delete by id or slup endpoint

--- src/api/controllers/schemaController.ts
+++ src/api/controllers/schemaController.ts
@@ -1,36 +1,42 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
 
-import * as schemaService from "../services/schemaService"
+import * as schemaService from "../services/schemaService";
 import { ResourceNotFoundError } from "../errors";
 
 export async function readAll(req: Request, res: Response) {
-  const schemas = await schemaService.list()
-  res.json(schemas)
+  const schemas = await schemaService.list();
+  res.json(schemas);
 }
 
 export async function readById(req: Request, res: Response) {
   const idOrSlug = req.params.id;
 
-  const schema = await schemaService.find(idOrSlug)
+  const schema = await schemaService.find(idOrSlug);
 
   if (schema === null)
-    throw new ResourceNotFoundError(`No such schema with ID or slug '${idOrSlug}'`)
+    throw new ResourceNotFoundError(
+      `No such schema with ID or slug '${idOrSlug}'`
+    );
 
-  res.json(schema)
+  res.json(schema);
 }
 
 export async function create(req: Request, res: Response) {
-  const id = await schemaService.create(req.body)
-  res.status(204)
-    .header("Location", `/schemas/${id}`)
-    .send()
+  const id = await schemaService.create(req.body);
+  res.status(204).header("Location", `/schemas/${id}`).send();
 }
 
 export async function deleteById(req: Request, res: Response) {
-  res.send("deleteById " + req.params.id)
+  res.send("deleteById " + req.params.id);
 }
 
 export async function deleteByIdOrSlug(req: Request, res: Response) {
   const idOrSlug = req.params.id;
-  res.send("deleteByIdOrSlug " + idOrSlug)
+  const schema = await schemaService.find(idOrSlug);
+
+  if (schema === null)
+    throw new ResourceNotFoundError(
+      `No such schema with ID or slug '${idOrSlug}'`
+    );
+  else res.send("deleteByIdOrSlug " + schema);
 }
