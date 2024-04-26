c875e5ce71929c21d4ede52d77ed1f33d27638a1
Srijan <srijan.vaddadi@gmail.com>
2024-04-26 11:09:19 +0100

    merge conflicts

--- src/api/controllers/schemaController.ts
+++ src/api/controllers/schemaController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-
-import * as schemaService from "../services/schemaService"
+import * as schemaService from "../services/schemaService";
 import { ResourceNotFoundError, PayloadError } from "../errors";
 
 export async function readAll(req: Request, res: Response) {
@@ -22,17 +21,20 @@ export async function readById(req: Request, res: Response) {
 }
 
 export async function create(req: Request, res: Response) {
-  const { slug, fields } = req.body
+  const { slug, fields } = req.body;
   if (typeof slug !== "string" || !Array.isArray(fields))
-    throw new PayloadError("Schema must have a slug and at least one field")
-  if (slug === "" || fields.length === 0 || slug === undefined || fields === undefined)
-    throw new PayloadError("Schema must have a slug and at least one field")
-  if ((fields.length) > 100 || slug.length > 100)
-    throw new PayloadError("Schema too large")
-  const id = await schemaService.create(req.body)
-  res.status(204)
-    .header("Location", `/schemas/${id}`)
-    .send()
+    throw new PayloadError("Schema must have a slug and at least one field");
+  if (
+    slug === "" ||
+    fields.length === 0 ||
+    slug === undefined ||
+    fields === undefined
+  )
+    throw new PayloadError("Schema must have a slug and at least one field");
+  if (fields.length > 100 || slug.length > 100)
+    throw new PayloadError("Schema too large");
+  const id = await schemaService.create(req.body);
+  res.status(204).header("Location", `/schemas/${id}`).send();
 }
 
 export async function deleteById(req: Request, res: Response) {
