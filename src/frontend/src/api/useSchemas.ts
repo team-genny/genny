e4f022447d410cf27ee50c54683fc18bbfa2440f
Sanju Ghandhi <ghandhisanju@gmail.com>
2024-05-02 16:08:45 +0100

    updates use schemaa

--- src/frontend/src/api/useSchemas.ts
+++ src/frontend/src/api/useSchemas.ts
@@ -1,4 +1,4 @@
-import useSWR, { mutate } from "swr";
+import useSWR from "swr";
 import fetcher from "./fetcher";
 import { Schema } from "../types";
 import SWRData from "./SWRData";
@@ -6,10 +6,11 @@ import SWRData from "./SWRData";
 export default function useSchemas(): SWRData<{ schemas: Schema[] }> & { mutate: (data?: any, shouldRevalidate?: boolean) => Promise<void> } {
   const { data, error, isLoading, mutate } = useSWR('/api/schemas', fetcher);
 
-  return {
-    schemas: data,
-    error,
-    isLoading,
-    mutate // Return the mutate function
-  };
+return {
+  schemas: data,
+  error,
+  isLoading,
+  mutate // Return the mutate function
+};
+
 }
