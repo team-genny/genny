423001040c61e34f53e03fbfea59d189f22eb10a
Jon Tyrrell <jon.Tyrrell@BAESystems.com>
2024-05-03 11:49:28 +0100

    Navbar-Hiding button

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
-import Sidebar from './components/Sidebar'
+
 
 function App() {
 
   return (
     <>
-      <Sidebar />
+
       <Outlet />
     </>
   )
--- src/frontend/src/components/Page.tsx
+++ src/frontend/src/components/Page.tsx
@@ -1,15 +1,39 @@
+import { useState } from "react"
 import clsx from "clsx"
 import "./Page.css"
-
+import Sidebar from "./Sidebar"
+import IconButton from "./IconButton"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 interface PageProps {
   className?: string
   children: React.ReactNode
 }
 
+
+
+
 export default function Page({ className, children }: PageProps) {
+
+
+
+  const [hideNavBar, setHideNavBar]=useState (false)
+
+    
+  function handleClick(){
+    setHideNavBar(!hideNavBar)
+  }
+
   return (
-    <main className={clsx("page", className)}>
-      {children}
-    </main>
-  )
-}
+    <>
+      {!hideNavBar && <Sidebar/>}
+      <main className={clsx("page", className)}>
+        {children}
+      </main>
+      <div className="navbar-ShowHide">
+      {hideNavBar
+        ? <IconButton icon={faBars} onClick={handleClick} />
+        : <IconButton icon={faBars} onClick={handleClick} />
+      }
+      </div>
+    </>
+  )}
