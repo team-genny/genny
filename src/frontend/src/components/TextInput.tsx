0dad962f2c509dc3aeb4a02b94c598a647bf6300
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-05-03 14:07:55 +0100

    Add disabled and name props to TextInput component

--- src/frontend/src/components/TextInput.tsx
+++ src/frontend/src/components/TextInput.tsx
@@ -8,13 +8,15 @@ interface TextInputProps {
   className?: string
   value?: string
   onChange?: (e: React.FormEvent<HTMLInputElement>) => void
+  disabled?: boolean
+  name?: string
 }
 
-export default function TextInput({ id, label, className, value, onChange }: TextInputProps) {
+export default function TextInput({ id, label, className, value, onChange, name, disabled }: TextInputProps) {
   return (
     <InputGroup className={clsx("input-group-text", className)}>
       <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} placeholder={label} value={value} onChange={onChange} />
+      <input type="text" id={id} placeholder={label} value={value} onChange={onChange} name={name} disabled={disabled} />
     </InputGroup>
   )
 }
