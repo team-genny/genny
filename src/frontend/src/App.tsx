26f117d4ee19a2f737fe20384f6fb843336abc0b
Aamirah <aamirahpatel@hotmail.co.uk>
2024-05-03 16:54:11 +0100

    error boundary fix

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,22 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import ErrorBoundary from "./components/ErrorBoundary";
-import CoinFlipGame from "./components/coinFlipGame"; // Corrected the import name
 
 function App() {
   return (
-    <Router>
       <ErrorBoundary>
-        <div className="app-container"> {/* Wrap the content in a div */}
           <Sidebar />
-          <Switch>
-            <Route path="/" Component={CoinFlipGame} /> {/* Use CoinFlipGame instead of coinFlipGame */}
-            {/* Add more routes as needed */}
-          </Switch>
-        </div>
       </ErrorBoundary>
-    </Router>
   );
 }
 
