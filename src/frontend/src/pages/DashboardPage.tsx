8205706baf3a30c15bcc377289a37974f4e29745
Sanju Ghandhi <ghandhisanju@gmail.com>
2024-05-03 09:56:08 +0100

    added current active schema count

--- src/frontend/src/pages/DashboardPage.tsx
+++ src/frontend/src/pages/DashboardPage.tsx
@@ -1,9 +1,15 @@
 import Page from "../components/Page";
+import useSchemas from "../api/useSchemas";
 
 export default function DashboardPage() {
+  const { schemas } = useSchemas();
+  
+  const totalActiveSchemas = schemas?.length ?? 0;
+
   return (
     <Page>
       <h1>Dashboard</h1>
+      <div>Total Active Schemas: {totalActiveSchemas}</div>
     </Page>
-  )
+  );
 }
