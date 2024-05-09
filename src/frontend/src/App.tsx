e1114c4c88a9a823ef86cb7b0c2b968b2b0f96ee
Aamirah <aamirahpatel@hotmail.co.uk>
2024-05-09 15:01:55 +0100

    fix sidebar render twice issue

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Sidebar from "./components/Sidebar";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
       <ErrorBoundary>
-          <Sidebar />
           <Outlet />
       </ErrorBoundary>
   );
