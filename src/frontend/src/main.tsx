65f6d04aea914faf91236ea34a870b175b764072
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-05-16 16:37:46 +0100

    compose all the things

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -9,6 +9,43 @@ import DataPage from "./pages/DataPage.tsx";
 import CreateSchemaPage from "./pages/CreateSchemaPage.tsx";
 import EditSchemaPage from "./pages/EditSchemaPage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
+
+import { Resource } from '@opentelemetry/resources';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import {
+  BatchSpanProcessor,
+  TracerConfig,
+  WebTracerProvider,
+} from '@opentelemetry/sdk-trace-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+
+const collectorOptions = {
+  url: 'http://localhost/traces',
+};
+
+const providerConfig: TracerConfig = {
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'genny',
+  }),
+};
+
+const provider = new WebTracerProvider(providerConfig);
+
+provider.addSpanProcessor(
+  new BatchSpanProcessor(new OTLPTraceExporter(collectorOptions)),
+);
+
+provider.register({
+  contextManager: new ZoneContextManager(),
+});
+
+registerInstrumentations({
+  instrumentations: [getWebAutoInstrumentations()],
+});
+
 const router = createBrowserRouter([
   {
     path: "/",
